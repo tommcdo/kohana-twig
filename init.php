7126af536b0c6cc04772abe8a9feadbbfbdd2826
Samuel Demirdjian <a@enov.ws>
2014-11-17 23:24:17 +0200

    Make Twig_Autoloader class available for composer-less projects

--- init.php
+++ init.php
@@ -1,4 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 define('TWIGPATH', __DIR__ . DIRECTORY_SEPARATOR);
+
+// Make the Twig_Autoloader class available for composer-less projects.
+if ( ! class_exists('Twig_Autoloader'))
+{
+	require_once 'vendor/twig/lib/Twig/Autoloader.php';
+}
+
 Twig::init();
