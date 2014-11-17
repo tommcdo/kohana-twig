6383af753680687956a418bba7ad665455465784
Samuel Demirdjian <sam@enov.ws>
2014-11-17 18:08:43 +0200

    use __DIR__ instead of __FILE__ for simplicity
    practically, __DIR__ will not contain the trailing slash
    so there is no need for `rtrim`.

--- init.php
+++ init.php
@@ -1,4 +1,4 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-define('TWIGPATH', rtrim(dirname(__FILE__), DIRECTORY_SEPARATOR).DIRECTORY_SEPARATOR);
+define('TWIGPATH', __DIR__ . DIRECTORY_SEPARATOR);
 Twig::init();
